eec99525e3b38144ebd2ebafee4d8760a3f12859
tyler <tyler.dojcak@gmail.com>
2021-02-25 16:33:23 -0500

    function to write categories to board spaces

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+let roundCategories = [];
+
+// FUNCTION TO RETRIEVE 6 RANDOM CATEGORIES FROM A RANDOM SET OF 100 
 function getCategories() {
     let categoriesUrl = "http://jservice.io/api/categories?count=100&offset=" + (Math.floor(Math.random() * 18250));
-    roundCategories = [];
     fetch(categoriesUrl).then(data => data.json()).then(categoriesList => {
         for (var i = 0; i < 6; i++) {
             let categoryIndex = Math.floor(Math.random() * categoriesList.length);
-            let category = categoriesList.pop(categoriesList
-                [categoryIndex])
-            roundCategories.push(category);
+            let category = categoriesList.pop(categoriesList[categoryIndex])
+            roundCategories.push(category.title);
             
         }
         console.log(roundCategories);
+        setCategories();
     });
 }
+
+// WRITE CATEGORIES TO CATEGORY BOARD SPACES
+function setCategories() {
+    let categorySpaces = document.getElementsByClassName("category");
+    for (var i = 0; i < 6; i++) {
+        categorySpaces[i].innerHTML = roundCategories[i].toUpperCase();
+    }
+    console.log(categorySpaces);
+}
+getCategories();
