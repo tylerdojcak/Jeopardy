c7c0b10c245fa2163676e24066877bee2f845978
tyler <tyler.dojcak@gmail.com>
2021-02-26 16:58:18 -0500

    add clues property with 5 clues to category objects

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 let roundCategories = [];
 
-// FUNCTION TO RETRIEVE 6 RANDOM CATEGORIES FROM A RANDOM SET OF 100 
+// RETRIEVE 6 RANDOM CATEGORIES FROM A RANDOM SET OF 100 
 function getCategories() {
     let categoriesUrl = "http://jservice.io/api/categories?count=100&offset=" + (Math.floor(Math.random() * 18250));
     fetch(categoriesUrl).then(data => data.json()).then(categoriesList => {
+        //console.log(categoriesList);
         for (var i = 0; i < 6; i++) {
             let categoryIndex = Math.floor(Math.random() * categoriesList.length);
-            let category = categoriesList.pop(categoriesList[categoryIndex])
-            roundCategories.push(category.title);
-            
+            let category = categoriesList.pop(categoriesList[categoryIndex]);
+            roundCategories.push(category);          
         }
-        console.log(roundCategories);
+        //console.log(roundCategories);
         setCategories();
+        getClues();
     });
 }
 
@@ -19,8 +20,28 @@ function getCategories() {
 function setCategories() {
     let categorySpaces = document.getElementsByClassName("category");
     for (var i = 0; i < 6; i++) {
-        categorySpaces[i].innerHTML = roundCategories[i].toUpperCase();
+        categorySpaces[i].innerHTML = roundCategories[i].title.toUpperCase();
     }
-    console.log(categorySpaces);
+    //console.log(categorySpaces);
 }
+
+
+
+
+// RETREIVE CLUES
+async function getClues() {
+    /*let url = "http://jservice.io/api/category?id=" + roundCategories[0].id;
+    fetch(url).then(data => data.json()).then(category => {
+        roundCategories[0].clues = category.clues.slice(0, 5);
+    })*/
+    for (var i in roundCategories) {
+        let url = "http://jservice.io/api/category?id=" + roundCategories[i].id;
+        await fetch(url).then(data => data.json()).then(category => {
+            roundCategories[i].clues = category.clues.slice(0, 5);
+            //console.log(category);
+        })
+    }
+    console.log(roundCategories);
+}
+
 getCategories();
