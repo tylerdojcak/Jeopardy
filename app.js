c768ebc1d41fb1e281f062c724d5ec689df68e77
tyler <tyler.dojcak@gmail.com>
2021-03-02 16:00:30 -0500

    some clean up

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ let roundClues = [];
 function getCategories() {
     let categoriesUrl = "http://jservice.io/api/categories?count=100&offset=" + (Math.floor(Math.random() * 18250));
     fetch(categoriesUrl).then(data => data.json()).then(categoriesList => {
-        //console.log(categoriesList);
         for (var i = 0; i < 6; i++) {
             let categoryIndex = Math.floor(Math.random() * categoriesList.length);
             let category = categoriesList.pop(categoriesList[categoryIndex]);
             roundCategories.push(category);          
         }
-        //console.log(roundCategories);
         setCategories();
         getClues();
     });
@@ -23,20 +21,14 @@ function setCategories() {
     for (var i = 0; i < 6; i++) {
         categorySpaces[i].innerHTML = roundCategories[i].title.toUpperCase();
     }
-    //console.log(categorySpaces);
 }
 
 // RETREIVE CLUES
 async function getClues() {
-    /*let url = "http://jservice.io/api/category?id=" + roundCategories[0].id;
-    fetch(url).then(data => data.json()).then(category => {
-        roundCategories[0].clues = category.clues.slice(0, 5);
-    })*/
     for (var i in roundCategories) {
         let url = "http://jservice.io/api/category?id=" + roundCategories[i].id;
         await fetch(url).then(data => data.json()).then(category => {
             roundCategories[i].clues = category.clues.slice(0, 5);
-            //console.log(category);
         })
     }
     console.log(roundCategories);
@@ -60,10 +52,7 @@ function assignClueIDs() {
         let categoryClues = document.getElementsByClassName(`category-${i}-clue`);
         for (var x = 0; x < 5; x++) {
             categoryClues[x].setAttribute("id", roundCategories[i].clues[x].id);
-            //console.log(categoryClues[x].id);
-            //console.log(roundCategories[i].clues);
         }
-        //console.log(categoryClues);
     }
 }
 
@@ -79,8 +68,6 @@ let hiddenAnswer;
 
 // SHOW MODAL
 function showModal() {
-    //this.getClue();
-    //console.log(this.getAttribute("id"));
     for (var i in roundClues) {
         if (this.getAttribute("id") == roundClues[i].id) {
             question.innerHTML = roundClues[i].question.toUpperCase();
